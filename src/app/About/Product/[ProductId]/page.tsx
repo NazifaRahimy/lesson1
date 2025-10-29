a53e0236d98b9819352f5f6cab086bd6ecc56a55
nazifaRahimi <nazifarahimy6@gmail.com>
2025-10-29 09:57:01 +0430

    feat: create dynamic route

---
+++ src/app/About/Product/[ProductId]/page.tsx
@@ -0,0 +1,23 @@
+import NotFundAbout from "../../not-found";
+interface ProductIdProps {
+  params: { ProductId: string };
+}
+
+const ProductId = ({ params }: ProductIdProps) => {
+  const { ProductId } = params;
+  if(parseInt(ProductId) > 50 ){
+     return NotFundAbout()
+  }
+
+  return (
+    <div className="w-full h-[400px] p-10 grid place-items-center">
+         <h1 className="text-2xl font-bold mb-4">صفحه محصول</h1>
+      <div className="border-2 border-dashed border-gray-500 p-10 rounded-xl text-center">
+       
+        <p className="text-lg">آیدی محصول: {ProductId}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductId;
