7596e211a0f8beb6d8bb64cc0723fd94496b5a82
nazifaRahimi <nazifarahimy6@gmail.com>
2025-10-29 09:39:47 +0430

    create auth group route with login, register and forget password pages

---
+++ src/app/(auth)/ForgetPassword/page.tsx
@@ -0,0 +1,7 @@
+const ForgetPassword = () => {
+    return ( 
+         <h1 className="my-20 text-center text-4xl font-semibold">Forget Passwor</h1>
+     );
+}
+ 
+export default ForgetPassword;
---
+++ src/app/(auth)/Register/page.tsx
@@ -0,0 +1,7 @@
+const Register = () => {
+    return ( 
+         <h1 className="my-20 text-center text-4xl font-semibold">Register</h1>
+     );
+}
+ 
+export default Register
