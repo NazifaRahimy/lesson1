6c98e43fa9b2278941294b4c7534ae5dea015fe0
nazifaRahimi <nazifarahimy6@gmail.com>
2025-10-29 10:00:23 +0430

    add nested dynamic route and not-found page

---
+++ src/app/About/Product/[ProductId]/[proId]/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFoundNestedId = () => {
+    return (  
+        <div className="w-full h-[400px]  gap-4 text-white flex-col bg-gray-400 flex items-center justify-center">
+        <h1 className="text-4xl font-semibold">404</h1>
+        <p className="text-2xl font-semibold"> Not found Nested Id Page</p>
+    </div>
+    );
+}
+ 
+export default NotFoundNestedId;
---
+++ src/app/About/Product/[ProductId]/[proId]/page.tsx
@@ -0,0 +1,28 @@
+import NotFoundNestedId from "./not-found";
+
+interface ProIdProps {
+    params: {ProductId: string; proId: string}
+}
+const ProId = ({params}: ProIdProps) => {
+    const {ProductId} = params;
+    const {proId} = params
+    
+    if(parseInt(proId) > 20){
+        return NotFoundNestedId()
+    }
+
+    return ( 
+    <div className="w-full h-[400px] p-10 grid place-items-center">
+        <h1 className="text-2xl font-bold mb-4">صفحه محصول</h1>
+        <div className="border-2 border-dashed border-gray-500 p-10 rounded-xl text-center">
+       
+        <p className="text-lg flex flex-col gap-4">
+            <span> آیدی محصول : {ProductId }</span>
+            <span>آیدی محصول تو در تو : {proId}</span>
+        </p>
+      </div>
+    </div>
+    );
+}
+ 
+export default ProId;
