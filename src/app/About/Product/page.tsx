e6de64c621db7f85f22e16f35eb203cc7c95e405
nazifaRahimi <nazifarahimy6@gmail.com>
2025-11-05 11:54:17 +0430

    fetch products data from API using axiso in client  Component

--- src/app/About/Product/page.tsx
+++ src/app/About/Product/page.tsx
@@ -1,41 +1,53 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Metadata } from "next";
+import axios from "axios";
 
 export const metadata: Metadata ={
    title: "Product", 
      description: "Discover our featured products, specifications, and details.",
 }
-const products = [
-  { id: "1", name: "Gol", price: "100 AF", image: "/gol.jpeg", time: "1 day ago" },
-  { id: 2, name: "Car", price: "10000 AF", image: "/foldar.jpeg", time: "1 day ago" },
-  { id: 3, name: "Caw", price: "50000 AF", image: "/caw.jpeg", time: "1 day ago" },
-  { id: 4, name: "زعفران", price: "100 AF", image: "/زعفران.jpeg", time: "1 day ago" },
-  { id: 5, name: "Sofa", price: "10000 AF", image: "/sofa.jpeg", time: "1 day ago" },
-  { id: 6, name: "Table", price: "10000 AF", image: "/table.jpeg", time: "1 day ago" },
-];
 
-const Product = () => {
+export interface productProps {
+  id: number,
+  title: string,
+  image: string,
+  description: string,
+  price: number;
+  category: string;
+  retaing: Retaing
+}
+export interface Retaing {
+  count: number,
+  rate: number
+}
+
+
+const Product =  async() => {
+  // const res = await fetch("https://fakestoreapi.com/products");
+  // const data = await res.json()
+  const {data} = await axios("https://fakestoreapi.com/products")
   return (
-    <div className="flex min-h-screen flex-col p-10">
+    <div className="flex  flex-col p-10">
       <h1 className="text-3xl font-bold mb-4 text-center">All Products</h1>
-       <div className="container bg-gray-100 grid grid-cols-4 gap-5 shadow-md p-4 mt-5">
-        {products.map((item) => (
+       <div className="container bg-gray-100 grid grid-cols-4 gap-5  shadow-md p-4 mt-5 ">
+        {data.map((item: productProps) => (
           <Link
             // href={`/Product/${item.id}`}
             href={`/About/Product/${item.id}`}
             key={item.id}
-            className="w-auto h-[350px] bg-white p-4 duration-200 transition-all hover:scale-105"
+            className="w-auto h-auto bg-white p-4 duration-200 transition-all hover:scale-105"
           >
             <div className="w-full h-[180px] relative overflow-hidden">
-              <Image src={item.image} alt={item.name} fill className="object-cover" />
+              <img src={item.image} alt={item.title}  className="object-cover" />
             </div>
-            <p className="mt-3 font-semibold flex justify-between">
-              {item.name} <span>{item.time}</span>
+            <p className="mt-3 font-semibold flex justify-between"> Title:
+              {item.title} 
             </p>
+            <span> category: {item.category}</span>
             <p className="font-semibold mt-1">{item.price}</p>
             <p className="text-sm text-gray-600 text-justify">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor.
+              {item.description}
             </p>
           </Link>
         ))}
